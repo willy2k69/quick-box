6d9a1ab15d5ddd4c9ecd4d81dea1fec94cf07f16
JMSDOnline <jmsolodesigns@gmail.com>
2016-01-07 20:23:10 -0600

    do not allow 12.04 and 14.04 for now

--- quickbox-apache.sh
+++ quickbox-apache.sh
@@ -435,7 +435,7 @@ function _intro() {
     echo "${dis}: ${alert} You do not appear to be running Ubuntu ${normal} "
     echo 'Exiting...'
     exit 1
-  elif [[ ! "${rel}" =~ ("12.04"|"14.04"|"15.04"|"15.10") ]]; then
+  elif [[ ! "${rel}" =~ ("15.04"|"15.10") ]]; then
     echo "${bold}${rel}:${normal} You do not appear to be running a supported Ubuntu release."
     echo 'Exiting...'
     exit 1
